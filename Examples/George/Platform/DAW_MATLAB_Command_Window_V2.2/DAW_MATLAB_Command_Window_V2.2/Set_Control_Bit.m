299523159cfcb349f93b3ae23310fa11a69fed79
ZGeorgeH <george-92@live.cn>
2018-07-30 13:37:21 +0100

    Revert "load the previous work to this branch"
    This reverts commit c66b5c29056dfddadffc157db67d168f5cb94b4e.

--- Examples/George/Platform/DAW_MATLAB_Command_Window_V2.2/DAW_MATLAB_Command_Window_V2.2/Set_Control_Bit.m
+++
@@ -1,8 +0,0 @@
-% This function write to the Control bit coil, refer to manual.
-% 2 input, (ON/OFF,Axis)
-% ON/OFF: 1 for MODBUS Control Enable, 0 for disable.
-
-function Set_Control_Bit(a,Axis)
-    global Controllers;
-    write(Controllers,'coils',1064,a,Axis); % Set control bit for given axis number
-end
